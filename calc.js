fbaa108bec3396661ff63e0ed4acafc4a2353f85
Megan <smith.megan.design@gmail.com>
2021-07-26 15:25:28 -0600

    adding further study problems

--- calc.js
+++ calc.js
@@ -15,15 +15,30 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 	if (mathSymbol === "+") {
+		//addition
 	console.log(num1+num2)
 	} else if (mathSymbol === "-") {
+		//subtraction
 		console.log(num1-num2)
-	} else if (mathSymbol === "x") {
+	} else if (mathSymbol === "*") {
+		//multiplication
 		console.log(num1*num2)
 	} else if (mathSymbol === "/") {
+		//division
 		console.log(num1/num2)
 	} else if (mathSymbol === "√") {
+		// square root
 		console.log(Math.sqrt(num1))
+	} else if (mathSymbol === "^") {
+		//exponential
+		console.log(Math.pow(num1,num2))
+	}	else if (mathSymbol === "**") {
+		//square a number
+		console.log(num1*num1)
+	} else if (mathSymbol === "^3") {
+		console.log(num1*num1*num1)
+	} else if (mathSymbol === "%") {
+		console.log(num1 % num2)
 	}
 	// This line closes the connection to the command line interface.
 	reader.close()
